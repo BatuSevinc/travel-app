290460d1a1844a82913d9841c140c88dd4522ca1
BatuSevinc <batusvnc1@gmail.com>
2023-12-28 22:52:10 +0300

    Project completed.
    Travel project completed with Next.js,TypeScript,TailwindCSS.

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Camp, Features, GetApp, Guide, Hero } from '@/components'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
+    </>
+  )
+}
+
+export default page
---
+++ components/index.js
@@ -0,0 +1,20 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Button from './Button'
+import Hero from './Hero'
+import Camp from './Camp'
+import Guide from './Guide'
+import GetApp from './GetApp'
+import Features from './Features'
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  Camp,
+  Hero,
+  Guide,
+  Features,
+  GetApp,
+  
+}
